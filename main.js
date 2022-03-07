cda48c4f64c504f87928eb864189849a3f623308
aleballabio <aleballabio05@gmail.com>
2022-03-07 14:59:19 +0100

    wrote steps for the exercise

---
+++ main.js
@@ -0,0 +1,29 @@
+/* Passaggi:
+
+1: Array di oggetti con i membri del team
+
+2: Funzione dell'array per stampare la relativa card con for in di: 
+    Nome 
+    Ruolo
+    Foto.
+
+    Funzione per le informazioni
+    1) Stampa del ciclo in un array vuoto
+
+2)  Ciclo While con arr.Team minore della lengh
+
+    1: arr vuoto per le info
+    2: contatore e contatore++
+    3: funzione  gather  info come variabile in array  [contatore]
+    4: Funzione della creazione card con accesso agli elementi dell'array creato 0 1 2
+
+3: For in di card per stampare le informazioni su elementi div in creazione
+
+    Funzione creazione Card con indice array
+
+    1: Creazione element Div con classe team card
+    2: altro element div con classe card img e aggiunta con append
+    3: creazione element con img e attributo in src con funzione per aggiunta immagini
+    4: Append su Card Image
+    5: Creazione div e card-text
+    6: Append del contenuto in card text
