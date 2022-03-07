5a3a578310b8e72c27cbd4eb499e738d1117e7ba
aleballabio <aleballabio05@gmail.com>
2022-03-07 17:12:57 +0100

    bonus done

--- main.js
+++ main.js
@@ -2,49 +2,48 @@ const arrTeam = [
     {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
-        Image: "img/wayne-barnett-founder-ceo.jpg"
+        Image: "wayne-barnett-founder-ceo.jpg"
 
     },
 
     {
         nome: "Angela Caroll",
         ruolo: "Chief Editor",
-        Image: "img/angela-caroll-chief-editor.jpg"
+        Image: "angela-caroll-chief-editor.jpg"
 
     },
 
     {
         nome: "Walter Gordon",
         ruolo: "Office Manager",
-        Image: "img/walter-gordon-office-manager.jpg"
+        Image: "walter-gordon-office-manager.jpg"
 
     },
 
     {
         nome: "Angela Lopez",
         ruolo: "Social Media Manager",
-        Image: "img/angela-lopez-social-media-manager.jpg"
+        Image: "angela-lopez-social-media-manager.jpg"
 
     },
 
     {
         nome: "Scott Estrada",
         ruolo: "Developer",
-        Image: "img/scott-estrada-developer.jpg"
+        Image: "scott-estrada-developer.jpg"
 
     },
 
     {
         nome: "Barbara Ramos",
         ruolo: "Graphic Designer",
-        Image: "img/barbara-ramos-graphic-designer.jpg"
+        Image: "barbara-ramos-graphic-designer.jpg"
 
     }
 ]
 
 for (let times = 0; times < arrTeam.length; times++) {
     const arrMember = GetDataMember(arrTeam[times]);
-    console.log(arrMember);
     const teamContainer = GetCardCreation(arrMember[0], arrMember[1], arrMember[2]);
 }
 
@@ -65,7 +64,7 @@ function GetCardCreation(name, role, image) {
     let card = `<div class="team-card">
             <div class="card-image">
               <img
-                src="${image}"
+                src="img/${image}"
                 alt="${name}"
               />
             </div>
@@ -76,4 +75,19 @@ function GetCardCreation(name, role, image) {
           </div>`;
 
     container.innerHTML += card;
+}
+
+// Bonus
+
+let btn = document.getElementById("addMemberButton");
+
+btn.addEventListener("click", addNewMemberCard);
+
+function addNewMemberCard() {
+    let inputName = document.getElementById("name").value;
+    let inputRole = document.getElementById("role").value;
+    let inputImage = document.getElementById("image").value;
+
+    GetCardCreation(inputName, inputRole, inputImage);
+
 }
